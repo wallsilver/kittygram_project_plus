9ec6386f43b1936cb39375bac541fe3bf32b99a4
Wallery Tsybrov <tsybrov.wallery@yandex.ru>
2025-08-30 17:16:22 +0300

    30.08.2025

--- cats/models.py
+++ cats/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+CHOICES = (
+        ('Gray', 'Серый'),
+        ('Black', 'Чёрный'),
+        ('White', 'Белый'),
+        ('Ginger', 'Рыжий'),
+        ('Mixed', 'Смешанный'),
+    )
+
 
 class Achievement(models.Model):
     name = models.CharField(max_length=64)
@@ -18,18 +26,19 @@ class Owner(models.Model):
 
 class Cat(models.Model):
     name = models.CharField(max_length=16)
-    color = models.CharField(max_length=16)
+    color = models.CharField(max_length=16, choices=CHOICES)
     birth_year = models.IntegerField()
-    # owner = models.ForeignKey(
-    #    Owner, related_name='cats', on_delete=models.CASCADE)
-    achievements = models.ManyToManyField(
-        Achievement, through='AchievementCat'
-    )
+    owner = models.ForeignKey(
+        Owner, related_name='cats', on_delete=models.CASCADE, blank=True,
+        null=True)
+    # Связь будет описана через вспомогательную модель AchievementCat
+    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
 
     def __str__(self):
         return self.name
 
 
+# В этой модели будут связаны id котика и id его достижения
 class AchievementCat(models.Model):
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
--- cats/serializers.py
+++ cats/serializers.py
@@ -1,37 +1,66 @@
 from rest_framework import serializers
+import datetime as dt
+import webcolors
 
 from .models import Cat, Owner, Achievement, AchievementCat
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class CatSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField(read_only=True)
-    achievements = AchievementSerializer(read_only=True, many=True)
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создаём запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        # Иначе делаем следующее:
         # Уберём список достижений из словаря validated_data и сохраним его
         achievements = validated_data.pop('achievements')
-
-        # Создадим нового котика пока без достижений, данных нам достаточно
+        # Сначала добавляем котика в БД
         cat = Cat.objects.create(**validated_data)
-
-        # Для каждого достижения из списка достижений
+        # А потом добавляем его достижения в БД
         for achievement in achievements:
-            # Создадим новую запись или получим существующий экземпляр из БД
             current_achievement, status = Achievement.objects.get_or_create(
                 **achievement)
-            # Поместим ссылку на каждое достижение во вспомогательную таблицу
-            # Не забыв указать к какому котику оно относится
+            # И связываем каждое достижение с этим котиком
             AchievementCat.objects.create(
                 achievement=current_achievement, cat=cat)
         return cat
